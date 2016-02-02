91b3f2d6188d25427ff1ef376b50038bcecf96a4
peterd <peter@dematte.at>
2016-02-02 17:18:14 +0100

    fixes position

--- javascript_implementation/jsColor.js
+++ javascript_implementation/jsColor.js
@@ -98,7 +98,7 @@
 				var onOff = off ? 'removeEventListener' : 'addEventListener',
 					focusListener = function(e) {
 						var input = this,
-							position = {left: input.offsetLeft, top: input.offsetTop},
+							position = window.ColorPicker.getOrigin(input),
 							index = multiple ? Array.prototype.indexOf.call(elms, this) : 0,
 							colorPicker = colorPickers[index] ||
 								(colorPickers[index] = createInstance(this, config)),
